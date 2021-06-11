4aa5f6c2e5303daae0efcc0dad3614c8579a5e23
leahzeisner <zeisner.l@northeastern.edu>
2021-06-11 00:01:39 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') form: NgForm;
+  defaultQuestion = 'pet'
+  answer = ''
+  genders = ['Female', 'Male', 'Nonbinary', 'Other']
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  submitted = false;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.form.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'Female'
+    // })
+    this.form.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form)
+  // }
+
+  onSubmit() {
+    this.submitted = true
+
+    this.user.username = this.form.value.userData.username
+    this.user.email = this.form.value.userData.email
+    this.user.secretQuestion = this.form.value.secret
+    this.user.answer = this.form.value.questionAnswer
+    this.user.gender = this.form.value.gender
+
+    this.form.reset();
+  }
+}
